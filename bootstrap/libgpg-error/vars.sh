2210a0d3cb053e2980da265507d86114ab607872
Olivier Kaloudoff <olivier.kaloudoff@gmail.com>
2018-09-01 03:16:41 +0200

    added libgpg-error (dependency for libgcrypt)

---
+++ bootstrap/libgpg-error/vars.sh
@@ -0,0 +1,5 @@
+DISTVER="libgpg-error-1.32"
+DISTSUFFIX="tar.gz"
+DISTFILES="ftp://ftp.gnupg.org/gcrypt/libgpg-error/$DISTVER.$DISTSUFFIX"
+UNPACKCOMD="tar -xzf"
+PATH=$PATH:/usr/local/Cellar/gettext/0.19.4/bin
