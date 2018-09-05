7370b7ed312bbe6c84df26c19681805dbf735e63
Olivier Kaloudoff <olivier.kaloudoff@gmail.com>
2018-09-05 22:52:27 +0200

    added cmus-2.8.0rc0 (needs a fix with pthread before pushing to master)

---
+++ bootstrap/cmus/build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# This code Copyright 2012 Todd Mortimer <todd.mortimer@gmail.com>
+#
+# You may do whatever you like with this code, provided the above
+# copyright notice and this paragraph are preserved.
+#
+# Olivier Kaloudoff <olivier.kaloudoff@gmail.com>, 2018
+#
+
+
+set -e
+source ../../lib.sh
+
+
+DISTVER="cmus"
+DISTSUFFIX="git"
+DISTFILES="https://github.com/$DISTVER/$DISTVER.git"
+GITVER="v2.8.0-rc0"
+UNPACKCOMD="tar -xJf"
+TASK=fetch
+package_init "$@"
+CONFIGURE_CMD="./configure 
+                CC=$PBTARGETARCH-gcc
+                "
+
+if [ "$TASK" == "fetch" ]
+then
+  cd "$WORKROOT"
+  # delete old version
+  rm -rf "$DISTVER"
+  git clone --recursive $DISTFILES $DISTVER
+  cd $DISTVER
+  git checkout $GITVER
+  cd "$WORKDIR"
+  TASK=patch
+fi
+
+package_patch
+package_build
+package_install
+package_bundle
+
