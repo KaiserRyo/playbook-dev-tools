c86e82c7f070717f5a2a0544233a2a74ff37281a
Olivier Kaloudoff <olivier.kaloudoff@gmail.com>
2018-09-20 09:30:21 +0200

    - libgcrypt/patches/: disable mpicalc binary, target not found - libgcrypt/build.sh: remove --with-libgpg-error-prefix option, unrecognized due to patch/configure-disable-gpg-error-detect.patch

--- bootstrap/libgcrypt/build.sh
+++ bootstrap/libgcrypt/build.sh
@@ -11,6 +11,7 @@ source ../../lib.sh
 
 DISTVER="libgcrypt"
 DISTSUFFIX="git"
+GITVER="libgcrypt-1.8.3"
 TASK=fetch
 
 package_init "$@"
@@ -20,13 +21,21 @@ CONFIGURE_CMD="./autogen.sh ;
                 --build=$PBBUILDARCH 
                 --target=$PBTARGETARCH 
                 --prefix=$PREFIX 
-		--with-libgpg-error-prefix=../libgpg-error-1.32
+		--disable-doc
                 CC=$PBTARGETARCH-gcc
-		CFLAGS=\"-I../libgpg-error-1.32/src -I../../libgpg-error-1.32/src \"
+		CFLAGS=\"-I../libgpg-error-1.32/src -I../../libgpg-error-1.32/src -fPIC -DENABLE_NEON_SUPPORT\"
 		LDFLAGS=\" -lgpg-error -lsocket -L/Users/testuser/bb/playbook-dev-tools/work/libgpg-error-1.32/src/.libs\"
                 MAKEINFO='/usr/bin/makeinfo --force'
                 "
 
+MYMAKEFLAGS=\"CC_FOR_BUILD=gcc\"
+
+# Flag not recognized
+#--with-libgpg-error-prefix=../libgpg-error-1.32
+
+# FIXME
+#  Absolute path in LDFLAGS was used due to an issue with libtool "not able to detect absolute path"
+#  We should use something like this instead
 #-L../libgpg-error-1.32/src/.libs
 
 if [ "$TASK" == "fetch" ]
@@ -36,6 +45,7 @@ then
   rm -rf "$DISTVER"
   git clone --recursive https://github.com/gpg/libgcrypt.git $DISTVER
   cd $DISTVER
+  git checkout $GITVER
   cd "$WORKDIR"
   TASK=patch
 fi
