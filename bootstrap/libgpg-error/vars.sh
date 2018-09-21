01068ea71ee2932dd0c4f592f4c42d78dd9b192e
Olivier Kaloudoff <olivier.kaloudoff@gmail.com>
2018-09-21 10:52:00 +0200

    - libgpg-error: merge vars.sh into build.sh

--- bootstrap/libgpg-error/vars.sh
+++
@@ -1,5 +0,0 @@
-DISTVER="libgpg-error-1.32"
-DISTSUFFIX="tar.gz"
-DISTFILES="ftp://ftp.gnupg.org/gcrypt/libgpg-error/$DISTVER.$DISTSUFFIX"
-UNPACKCOMD="tar -xzf"
-PATH=$PATH:/usr/local/Cellar/gettext/0.19.4/bin
